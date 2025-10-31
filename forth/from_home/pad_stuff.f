d09b7fac63a7f124f26f31c05780d5fb0f3cc859
tomdowdy <tomdowdy1994@gmail.com>
2025-10-31 05:43:34 -0500

    python and forth stuff

---
+++ forth/from_home/pad_stuff.f
@@ -0,0 +1,10 @@
+marker pad-stuff
+
+: ]pada pad ! ;
+: ]padb pad cell + ! ;
+: ]padc pad 2 cells + ! ;
+: ]padd pad 3 cells + ! ;
+: pada] pad @ ;
+: padb] pad cell + @ ;
+: padc] pad 2 cells + @ ;
+: padd] pad 3 cells + @ ;
