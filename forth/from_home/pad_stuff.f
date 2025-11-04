add9be767dda5d78ef9f2b765c41be5b910ad30f
tomdowdy <tomdowdy1994@gmail.com>
2025-11-04 06:46:44 -0600

    Update pad_stuff.f

--- forth/from_home/pad_stuff.f
+++ forth/from_home/pad_stuff.f
@@ -1,9 +1,9 @@
 \ marker pad-stuff
 
-: ]pada pad ! ;
-: ]padb pad cell + ! ;
-: ]padc pad 2 cells + ! ;
-: ]padd pad 3 cells + ! ;
+: ]pada depth 0<> if pad ! then ;
+: ]padb depth 0<> if pad cell + ! then ;
+: ]padc depth 0<> if pad 2 cells + ! then ;
+: ]padd depth 0<> if pad 3 cells + ! then ;
 : pada] pad @ ;
 : padb] pad cell + @ ;
 : padc] pad 2 cells + @ ;
