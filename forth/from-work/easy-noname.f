4902c3e9876485fce05e2547ca04a0fbb9d13843
Tom Dowdy <tomdowdy1994@gmail.com>
2026-01-28 15:05:33 -0600

    Update easy-noname.f

--- forth/from-work/easy-noname.f
+++ forth/from-work/easy-noname.f
@@ -1,12 +1,44 @@
 0 value active-noname
 0 value noname-active
-: noname: noname-active 0= if here to noname-active :noname else cr abort" Noname already active." then ;
-: noname; postpone ; dup to active-noname ; immediate
-: noname- 
-	noname-active 0= if 
-		cr abort" Noname not active." 
-	else 
-		noname-active dp ! 0 to noname-active 
+0 value compiling-noname
+: reset-noname 
+		0 to compiling-noname
 		0 to active-noname
+		0 noname-active
+;
+: noname: noname-active 
+	if 
+		cr abort" Noname already active." 
+	else 
+		here to noname-active :noname 
+		1 to compiling-noname 
 	then 
 ;
+: noname; compiling-noname
+	if 
+		postpone ; 
+		dup to active-noname 
+		0 to compiling-noname
+	else
+		cr abort" Not compiling a noname."
+		reset-noname
+	then
+; immediate
+: noname- 
+	noname-active if 
+		noname-active dp ! 
+		reset-noname
+	else 
+		cr abort" Noname not active." 
+		reset-noname
+	then 
+;
+: noname active-noname execute ;
+: ; compiling-noname 
+	if 
+		cr abort" Compiling noname, use noname;." 
+		reset-noname
+	else 
+		postpone ; 
+	then 
+; immediate
