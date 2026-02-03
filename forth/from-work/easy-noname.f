53dc66104f2d8f7e77c686f2a4fd071255acd8a3
Tom Dowdy <tomdowdy1994@gmail.com>
2026-02-03 15:45:30 -0600

    yhnb

--- forth/from-work/easy-noname.f
+++ forth/from-work/easy-noname.f
@@ -1,65 +1,81 @@
-0 value active-noname
+0 value active.noname
 \ stores prior dictionary top and indicates an active noname
-0 value noname-active 
-0 value compiling-noname
-: _reset-noname 
-		0 to compiling-noname
-		0 to active-noname
-		0 to noname-active
+0 value noname.active 
+0 value compiling.noname
+0 value noname.base
+: _reset.noname 
+		0 to compiling.noname
+		0 to active.noname
+		0 to noname.active
 ;
-: noname: noname-active 
+: noname: noname.active 
 	if 
 		cr s" Noname already active." cr
 		22 throw
 	else 
-		here to noname-active 
-		1 to compiling-noname 
+		here to noname.active 
+		1 to compiling.noname 
 		:noname 
 	then 
 ;
-: noname; compiling-noname
+: noname; compiling.noname
 	if 
 		postpone ; 
-		dup to active-noname 
-		0 to compiling-noname
+		dup to active.noname 
+		0 to compiling.noname
 	else
-		cr s" Not compiling a noname." cr
-		_reset-noname
+		cr ." Not compiling a noname." cr
+		_reset.noname
 		23 throw
 	then
 ; immediate
 : noname- 
-	noname-active if 
-		noname-active dp ! 
-		_reset-noname
+	here noname.base = if
+		cr ." No nonames available." cr
+		24 throw
+	then
+	noname.active if 
+		noname.active dp ! 
+		_reset.noname
 	else 
 		cr ." Noname not active." cr
-		_reset-noname
+		_reset.noname
 		22 throw
 	then 
 ;
-: noname active-noname execute ;
-: ; compiling-noname 
+: noname active.noname execute ;
+: ; compiling.noname 
 	if 
 		cr ." Compiling noname, use noname;." cr
-		noname;
+		postpone noname;
 	else 
 		postpone ; 
 	then 
 ; immediate
-: noname-bak
-	noname-active if
-		noname-active ,
-		active-noname ,
+: noname.bak
+	here noname.base = if
+		cr ." No nonames available." cr
+		24 throw
+	then
+	noname.active if
+		noname.active ,
+		active.noname ,
+		_reset.noname
 	else
 		cr ." Noname not active." cr
 		22 throw
 	then
 ;
-: noname-restore
-	here cell - @ active-noname !
+: noname.restore
+	here noname.base = if
+		cr ." No nonames available." cr
+		24 throw
+	then
+	noname-
+	here cell - @ to active.noname
 	0 cell - allot
-	here cell - @ noname-active !
+	here cell - @ to noname.active
 	0 cell - allot
 ;
+here to noname.base
 
