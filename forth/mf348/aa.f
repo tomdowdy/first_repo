c54da366f0567e71a5b57e29358dbb1c4726c9af
tomdowdy <tomdowdy1994@gmail.com>
2026-06-11 08:43:18 -0500

    Update aa.f

--- forth/mf348/aa.f
+++ forth/mf348/aa.f
@@ -46,12 +46,16 @@
 : hinit ( n -- ) here cells allot , ; \ create a buffer of n cells with start pointer at 'here - cell'
 : hcleanup ( n -- ) 1 + 0 swap - allot ; \ releases memory used for buffer
 : hoff cells hp + ; \ hoff = here offset
-: h0 hp ;
-: h1 hp cell + ;
-: h2 2 hoff ;
-: h3 3 hoff ;
-: h4 4 hoff ;
-
+: h0 hp @ ;
+: h1 hp cell + @ ;
+: h2 2 hoff @ ;
+: h3 3 hoff @ ;
+: h4 4 hoff @ ;
+: h0, hp ! ;
+: h1, hp cell + ! ;
+: h2, 2 hoff ! ;
+: h3, 3 hoff ! ;
+: h4, 4 hoff ! ;
 \ -------------------------------------------------------------------
 
 : nrev ( n ) \ reverse n stack items.
