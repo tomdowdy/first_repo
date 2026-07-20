0f9822f018732efcb501c249c81e19512e93b087
tomdowdy <tomdowdy1994@gmail.com>
2026-07-19 20:28:47 -0500

    Update aa.f

--- forth/mf348/aa.f
+++ forth/mf348/aa.f
@@ -49,23 +49,8 @@
 \ hp retrieves that pointer from which memory operations can start.
 \ do a '0 cell - allot -22 allot' to clean up. Alternately '-23 allot' cleans up.
 
-<<<<<<< HEAD
 : hinit ( n -- ) here swap cells allot , ; \ create a buffer of n cells with start pointer at 'here - cell'
 : hcleanup ( n -- ) 1 + cells 0 swap - allot ; \ releases memory used for buffer
-=======
-\ hinit creates a buffer of n cells with start pointer at cell prior to here.
-\ hinit makes sure the input parameter is not 0 and is not greater than 256.
-\ If using the h structure, you cannot push more onto the dictionary stack since
-\ the structure start pointer resides in the cell prior to here.
-\ You can create as many of the h structures as desired with the restriction that
-\ only the last structure can be used and you must do an hreset to access the one
-\ below the structure on the top of the dictionary stack.
-
-: hinit ( n -- ) depth 0= if 888 throw then dup 256 > if 889 throw then here swap cells allot , ; 
-
-\ : hreset ( n -- ) hp here swap - 0 swap - allot ; \ releases memory used for buffer
-: hreset ( -- ) 0 here here- @ - - allot ;
->>>>>>> e9c5d76336ba1786494a61dae50d72f3752ef4f2
 : hoff cells hp + ; \ hoff = here offset
 : h0 hp @ ;
 : h1 hp cell + @ ;
@@ -86,8 +71,8 @@
 \ use like :noname and ; but start of anonymous function is stored
 \ at here 2 cells - and xt is stored at here cell -
 \ anon pulls xt address so @ execute will run item it
-: don here :noname ; \ for do anonymous (create a new anonymous function)
-: eon postpone ; swap , , ; immediate
+: dan here :noname ; \ for do anonymous (create a new anonymous function)
+: ean postpone ; swap , , ; immediate
 : ranon here 2 cells - @ here - allot ; \ reset anonymous structure
 
 
